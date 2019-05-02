524605db19c59710be0256e9d50a1f08dadb1680
Alejandro Perez <alex2721ao@gmail.com>
2019-05-02 10:50:21 -0700

    Changes that might be necessary

--- memory.cpp
+++ memory.cpp
@@ -15,13 +15,21 @@ struct Process
     int page = 0;
 };
 
+struct Page//Maybe something we use??
+{
+    bool taken = true;
+    Process indiv_process;
+};
+
 void FIFO(vector<Process>vec);
 void LRU(vector<Process>vec);
 void Random(vector<Process>vec);
 
 int main()
 {
+    //Rather than have a 2d vector and things more compilcated I created a vector of structs so now each element has an ID, action, and a page.
     vector<Process>processes;
+    vector<bool>pages(20);//The amount of pages we're restricted to.  Not sure if its supposed to be a bool but that's my guess for now.  Might actually have to create another struct
     Process proc;
     ifstream process_list;
     string line;
@@ -38,12 +46,17 @@ int main()
         if(count>=100){break;}
     }
     process_list.close();
-
-    for(int i=0; i<processes.size(); i++)
-    {
-        cout << processes[i].process_id << " " << processes[i].action << " " << processes[i].page << endl;
-    }
-
-
 return 0;
 }
+
+
+void FIFO(vector<Process>vec)
+{
+
+}
+
+//For testing output of the vector
+    // for(int i=0; i<processes.size(); i++)
+    // {
+    //     cout << processes[i].process_id << " " << processes[i].action << " " << processes[i].page << endl;
+    // }
