91f9afb4e86a8685caff75cbca6156a7cbda4468
Alejandro Perez <alex2721ao@gmail.com>
2019-05-02 09:09:56 -0700

    Added .dat file

--- memory.cpp
+++ memory.cpp
@@ -1,8 +1,16 @@
-#include<iostream>
+#include <iostream>
+#include<string>
+#include<vector>
+//#include<utility>
+#include<algorithm>
+#include<fstream>
+#include <sstream>
 
 using namespace std;
 
 int main()
 {
-    
+    vector<vector<string> >processes;
+
+
 }
