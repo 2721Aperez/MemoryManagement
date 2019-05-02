3f97adb9c18e23c2fd090ba2ea7412cd157ef931
Alejandro Perez <alex2721ao@gmail.com>
2019-05-02 10:38:28 -0700

    Able to read in the file and store into a vector

--- memory.cpp
+++ memory.cpp
@@ -8,9 +8,36 @@
 
 using namespace std;
 
+struct Process
+{
+    int process_id;
+    char action;
+    int page = 0;
+};
+
 int main()
 {
-    vector<vector<string> >processes;
+    vector<Process>processes;
+    Process proc;
+    ifstream process_list;
+    string line;
+    int count = 0;
+
+    process_list.open("memory.dat");
+    while(getline(process_list, line))
+    {
+        istringstream iss(line);
+        iss >> proc.process_id >> proc.action >> proc.page;
+        if(proc.action == 'C' || proc.action == 'T'){proc.page = 0;}
+        processes.push_back(proc);
+        //cout << processes[count].process_id << " " << processes[count].action << " " << processes[count].page << endl;
+        count++;
+        if(count>=100){break;}
+    }
+    process_list.close();
+
+    cout << processes.size();
 
 
+return 0;
 }
