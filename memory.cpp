871c677a50471b4e177928629014d5b893f16087
raymondchen513 <39606035+raymondchen513@users.noreply.github.com>
2019-05-06 21:18:31 -0700

    Changed Page struct, added print function, worked on FIFO
    -Page struct: added physAddr and virtAddr
    -print function will print out contents of physical memory
    -FIFO: Actions "A"llocate and "T"erminate implemented

--- memory.cpp
+++ memory.cpp
@@ -17,13 +17,18 @@ struct Process
 
 struct Page//Maybe something we use??
 {
-    bool taken = true;
+    bool taken = false;
+    int physAddr = -1;
+    int virtAddr = -1;
     Process indiv_process;
 };
 
 void FIFO(vector<Process>vec);
 void LRU(vector<Process>vec);
 void Random(vector<Process>vec);
+//Process createProcess(vector<Process>vec);
+void printPhysMem(vector<Page> memory);
+
 
 //Process Format: PROCESS_ID    ACTION  PAGE
 //Process_ID: numeric value corresponding to process running on the system
@@ -63,18 +68,40 @@ return 0;
 
 void FIFO(vector<Process>vec/*, vector<bool>& pageTable*/)
 {
-    vector<Process> pageQueue;
+    vector<Page> swapSpace; // "Infinite" swap space
+    //vector<Pages> pageTable; // Page table for a specific process
+    vector<Page> physicalMem(20); // Physical Memory pages
+    cout << "Physical Memory size = " << physicalMem.size() << " Physical Memory Capacity = " << physicalMem.capacity() << endl;
+    //vector<vector<Process>> physicalMem;
+    vector<Page> pageQueue(20); // Not needed?
     int pageIndex = 0;
     //bool emptyPage = false;
-
+    //vector<
     for(int i = 0; i < vec.size(); i++) {
         
         switch(vec[i].action) {
             case 'C': cout << "Process " << vec[i].process_id << " created" << endl;
+                      //pageQueue.push_back(vec[i]);
+                      
                       break;
             case 'T': cout << "Process " << vec[i].process_id << " terminated" << endl;
+                      //Free all the pages of the terminated process
+                      for(int j = 0; j < physicalMem.size(); j++) {
+                          if(physicalMem[j].indiv_process.process_id == vec[i].process_id) {
+                              physicalMem[j].taken = false;
+                              physicalMem[j].physAddr = -1;
+                              physicalMem[j].virtAddr = -1;
+                          }
+                      }
                       break;
             case 'A': cout << "Process " << vec[i].process_id << " allocated memory at address " << vec[i].page << endl;
+                      if(pageIndex != physicalMem.capacity()) {
+                          physicalMem[pageIndex].taken = true;
+                          physicalMem[pageIndex].indiv_process = vec[i];
+                          physicalMem[pageIndex].virtAddr = vec[i].page;
+                          physicalMem[pageIndex].physAddr = pageIndex;
+                          pageIndex++;
+                      }
                       break;
             case 'R': cout << "Process " << vec[i].process_id << " read " << vec[i].page << endl;
                       break;
@@ -84,6 +111,7 @@ void FIFO(vector<Process>vec/*, vector<bool>& pageTable*/)
                       break;
             default: cout << "Invalid process action" << endl;
         }
+        printPhysMem(physicalMem);
         
         // if(vec[i].action == 'A') {
         //     pageQueue.push_back(vec[i]);
@@ -102,6 +130,17 @@ void FIFO(vector<Process>vec/*, vector<bool>& pageTable*/)
     }
 
 
+}
+
+void printPhysMem(vector<Page> memory) {
+    cout << "PHYSICAL" << endl;
+    for(int i = 0; i < memory.capacity(); i++) {
+        cout << "\t" << i << "\t";
+        if(!memory[i].taken) { cout << "FREE" << endl; }
+        else {
+            cout << "Process\t" << memory[i].indiv_process.process_id << endl;
+        }
+    }
 }
 
 //For testing output of the vector
